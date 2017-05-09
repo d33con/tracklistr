a308e5ff56153871567e3f10d2f203d2a1f5780d
Oli Bullen <d33con@gmail.com>
2017-05-09 16:43:54 +0900

    functional component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import React, { Component } from 'react';
+// @flow
+import React from 'react';
 import { Container } from 'semantic-ui-react';
 
 import AudioPlayer from './components/AudioPlayer';
@@ -7,21 +8,19 @@ import TracklistTable from './components/TracklistTable';
 
 import './style/App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="b-app">
-        <div className="b-app-header">
-          <div className="b-app-header--title">Tracklistr</div>
-        </div>
-        <Container className="b-app-body">
-          <AudioPlayer />
-          <SearchBox />
-          <TracklistTable />
-        </Container>
+function App() {
+  return (
+    <div className="b-app">
+      <div className="b-app-header">
+        <div className="b-app-header--title">Tracklistr</div>
       </div>
-    );
-  }
+      <Container className="b-app-body">
+        <AudioPlayer />
+        <SearchBox />
+        <TracklistTable />
+      </Container>
+    </div>
+  );
 }
 
 export default App;
