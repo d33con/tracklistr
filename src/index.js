20f2345d89b76df8f4c3d77220b3fb367c510e4b
Oli Bullen <d33con@gmail.com>
2017-05-09 16:43:41 +0900

    eslint

--- src/index.js
+++ src/index.js
@@ -1,10 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
 import 'semantic-ui-css/semantic.min.css';
 import './index.css';
+import App from './App';
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
+ReactDOM.render(<App />, document.getElementById('root'));
