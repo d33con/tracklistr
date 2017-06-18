a57ce15693cc16555bc9f5f365c5c9a536b6a2d8
Oli Bullen <d33con@gmail.com>
2017-06-18 22:14:16 +0900

    addReleaseToTracklist

--- src/components/Search/TrackDetailsDropdown.js
+++ src/components/Search/TrackDetailsDropdown.js
@@ -1,34 +1,62 @@
 import React from "react";
 import PropTypes from "prop-types";
-import { List, Icon } from "semantic-ui-react";
+import { Button, List, Icon } from "semantic-ui-react";
 import v4 from "uuid";
 
 function TrackDetailsDropdown(props) {
   const { tracklist } = props.track;
+  function addReleaseToTracklist(artist, title, label, id) {
+    const trackTitle = `${artist} - ${title}`;
+    console.log(tracklist);
+    const track = {
+      trackTitle,
+      trackUrl: props.track.uri.slice(23),
+      trackLabel: label[0],
+      releaseId: id
+    };
+
+    props.addReleaseToTracklist(track);
+  }
   return (
     <div>
 
       {tracklist.map((track, singleArtist) => {
         singleArtist = props.track.artists[0].name;
+        const artists = track.artists && track.artists.length > 0
+          ? track.artists.map(name => {
+              return name.join !== ","
+                ? `${name.name} ${name.join} `
+                : `${name.name}`;
+            })
+          : singleArtist;
         return (
           <List.Item key={v4()}>
             {track.position}
             .
             {" "}
-            {track.artists && track.artists.length > 0
-              ? track.artists.map(name => name.name)
-              : singleArtist}
+            {artists}
             {" "}
             -
             {" "}
             {track.title}
-            <Icon
-              name="plus square outline"
+            <Button
+              animated="fade"
+              size="tiny"
               color="blue"
-              link
-
-              //onClick={() => addReleaseToTracklist({ result })}
-            />
+              onClick={() =>
+                addReleaseToTracklist(
+                  artists,
+                  track.title,
+                  props.label,
+                  props.track.id,
+                  track
+                )}
+            >
+              <Button.Content hidden>Add</Button.Content>
+              <Button.Content visible>
+                <Icon name="plus" />
+              </Button.Content>
+            </Button>
           </List.Item>
         );
       })}
