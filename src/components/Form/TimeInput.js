0698ed67d22b24b5bb0c785aaa68824281eee249
Oli Bullen <d33con@gmail.com>
2017-06-18 22:12:10 +0900

    simplify

--- src/components/Form/TimeInput.js
+++ src/components/Form/TimeInput.js
@@ -2,31 +2,17 @@ import React from "react";
 import PropTypes from "prop-types";
 import { Form } from "semantic-ui-react";
 
-function TimeInput({ label, value, name, type, step, min, max, onChange }) {
+function TimeInput({ label, value }) {
   return (
     <div>
-      <Form.Input
-        label={label}
-        value={value}
-        name={name}
-        type={type}
-        step={step}
-        min={min}
-        onChange={onChange}
-      />
+      <Form.Input label={label} value={value} />
     </div>
   );
 }
 
 TimeInput.propTypes = {
   label: PropTypes.string.isRequired,
-  value: PropTypes.oneOfType([PropTypes.number, PropTypes.string]).isRequired,
-  name: PropTypes.string.isRequired,
-  type: PropTypes.string.isRequired,
-  step: PropTypes.string.isRequired,
-  min: PropTypes.string.isRequired,
-  max: PropTypes.string,
-  onChange: PropTypes.func.isRequired
+  value: PropTypes.oneOfType([PropTypes.string, PropTypes.number]).isRequired
 };
 
 export default TimeInput;
