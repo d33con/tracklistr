11f039f8a91851bb431ec0bee587d573ec405ba6
Oli Bullen <d33con@gmail.com>
2017-07-17 22:49:04 +0900

    add styling

--- src/components/AudioPlayer/AddTrackModal.js
+++ src/components/AudioPlayer/AddTrackModal.js
@@ -1,20 +1,28 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { Button, Modal } from "semantic-ui-react";
-import convertTimeToString from "../../HelperFunctions/ConvertTimeToString";
 
+import convertTimeToString from "../../HelperFunctions/ConvertTimeToString";
 import SearchBox from "../Search/SearchBox";
+
+import "../../style/AddTrackModal.css";
 
 function AddTrackModal({ shown, onClose, currentTime, addReleaseToTracklist }) {
   return (
-    <Modal size="fullscreen" open={shown} onClose={onClose} dimmer>
-      <Modal.Header>
-        Add New Track at {convertTimeToString(currentTime)}
+    <Modal
+      size="fullscreen"
+      open={shown}
+      onClose={onClose}
+      dimmer
+      className="b-modal"
+    >
+      <Modal.Header className="b-modal-header">
+        Adding New Track at {convertTimeToString(currentTime)}
       </Modal.Header>
-      <Modal.Content>
+      <Modal.Content className="b-modal-content">
         <SearchBox addReleaseToTracklist={addReleaseToTracklist} />
       </Modal.Content>
-      <Modal.Actions>
+      <Modal.Actions className="b-modal-footer">
         <Button
           negative
           onClick={onClose}
