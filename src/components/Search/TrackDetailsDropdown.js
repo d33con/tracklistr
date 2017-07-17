2ef3e958a69ba7c8671fef8b783d9ff56fdd7c3b
Oli Bullen <d33con@gmail.com>
2017-07-17 22:53:14 +0900

    styling. correct display of multiple artists

--- src/components/Search/TrackDetailsDropdown.js
+++ src/components/Search/TrackDetailsDropdown.js
@@ -3,11 +3,12 @@ import PropTypes from "prop-types";
 import { Button, List, Icon } from "semantic-ui-react";
 import v4 from "uuid";
 
+import "../../style/TrackDetailsDropdown.css";
+
 function TrackDetailsDropdown({ addReleaseToTracklist, label, track }) {
   const { tracklist, uri } = track;
   function sendReleaseToTracklist(artist, title, label, id, position) {
     const trackTitle = `${artist} - ${title}`;
-    console.log(tracklist);
     const track = {
       trackTitle,
       trackUrl: uri.slice(23),
@@ -19,30 +20,27 @@ function TrackDetailsDropdown({ addReleaseToTracklist, label, track }) {
   }
   return (
     <div>
-
       {tracklist.map((singleTrack, singleArtist) => {
         singleArtist = track.artists[0].name;
-        const artists = track.artists && track.artists.length > 0
-          ? track.artists.map(name => {
-              return name.join === ","
-                ? `${name.name} ${name.join} `
-                : `${name.name}`;
-            })
-          : singleArtist;
+        const artists =
+          track.artists && track.artists.length > 1
+            ? track.artists.map(name => {
+                return name.join.length > 0
+                  ? `${name.name} ${name.join} `
+                  : `${name.name}`;
+              })
+            : singleArtist;
         return (
-          <List.Item key={v4()}>
+          <List.Item key={v4()} className="b-track-details-list-item">
             {singleTrack.position}
-            .
-            {" "}
-            {artists}
-            {" "}
-            -
-            {" "}
-            {singleTrack.title}
+            . {artists} - {singleTrack.title}
             <Button
               animated="fade"
-              size="tiny"
+              size="mini"
               color="blue"
+              inverted
+              style={{ marginLeft: "0.5em" }}
+              className="b-track-details-list-item-button"
               onClick={() =>
                 sendReleaseToTracklist(
                   artists,
