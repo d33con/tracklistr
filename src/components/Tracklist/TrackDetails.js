618479eae4f1ae1dad8bb69543658c219af169f5
Oli Bullen <d33con@gmail.com>
2017-08-10 22:34:21 +0900

    remove onChange

--- src/components/Tracklist/TrackDetails.js
+++ src/components/Tracklist/TrackDetails.js
@@ -3,20 +3,11 @@ import { Form } from "semantic-ui-react";
 
 import TimeInput from "./Form/TimeInput";
 
-const onChange = () => {
-  console.log("changed");
-};
-
 function TrackDetails({ trackTime, currentlySelectedSearchResult }) {
   return (
     <Form>
       <Form.Group widths="equal">
-        <TimeInput
-          label="Time"
-          value={trackTime}
-          name="Time"
-          onChange={onChange}
-        />
+        <TimeInput label="Time" value={trackTime} />
         <Form.Input label="Title" value="Title" />
         <Form.Input label="Label" />
         <Form.Input label="Track Link" />
