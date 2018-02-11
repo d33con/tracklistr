0400378d3a61f8401e83df5975fba2e8f03c5eec
Oli Bullen <d33con@gmail.com>
2018-02-11 12:51:25 +0000

    class component

--- src/components/Search/TrackDetailsDropdown.js
+++ src/components/Search/TrackDetailsDropdown.js
@@ -1,13 +1,16 @@
-import React from "react";
+import React, { Component } from "react";
+import { observer, inject } from "mobx-react";
 import PropTypes from "prop-types";
 import { Button, List, Icon } from "semantic-ui-react";
 import v4 from "uuid";
 
 import "../../style/TrackDetailsDropdown.css";
 
-function TrackDetailsDropdown({ addReleaseToTracklist, label, track }) {
-  const { tracklist, uri } = track;
-  function sendReleaseToTracklist(artist, title, label, id, position) {
+@inject("store")
+@observer
+class TrackDetailsDropdown extends Component {
+  sendReleaseToTracklist = (artist, title, label, id, position) => {
+    const { uri } = this.props.track;
     const trackTitle = `${artist} - ${title}`;
     const track = {
       trackTitle,
@@ -16,55 +19,59 @@ function TrackDetailsDropdown({ addReleaseToTracklist, label, track }) {
       releaseId: `${id}${position}`
     };
 
-    addReleaseToTracklist(track);
+    this.props.store.addReleaseToTracklist(track);
+  };
+  render() {
+    const { track, label } = this.props;
+    const { tracklist } = this.props.track;
+    return (
+      <div>
+        {tracklist.map((singleTrack, singleArtist) => {
+          singleArtist = track.artists[0].name;
+          const artists =
+            track.artists && track.artists.length > 1
+              ? track.artists.map(name => {
+                  return name.join.length > 0
+                    ? `${name.name} ${name.join} `
+                    : `${name.name}`;
+                })
+              : singleArtist;
+          return (
+            <List.Item key={v4()} className="b-track-details-list-item">
+              {singleTrack.position}
+              . {artists} - {singleTrack.title}
+              <Button
+                animated="fade"
+                size="mini"
+                color="blue"
+                inverted
+                style={{ marginLeft: "0.5em" }}
+                className="b-track-details-list-item-button"
+                onClick={() =>
+                  this.sendReleaseToTracklist(
+                    artists,
+                    singleTrack.title,
+                    label,
+                    track.id,
+                    singleTrack.position
+                  )
+                }
+              >
+                <Button.Content hidden>Add</Button.Content>
+                <Button.Content visible>
+                  <Icon name="plus" />
+                </Button.Content>
+              </Button>
+            </List.Item>
+          );
+        })}
+      </div>
+    );
   }
-  return (
-    <div>
-      {tracklist.map((singleTrack, singleArtist) => {
-        singleArtist = track.artists[0].name;
-        const artists =
-          track.artists && track.artists.length > 1
-            ? track.artists.map(name => {
-                return name.join.length > 0
-                  ? `${name.name} ${name.join} `
-                  : `${name.name}`;
-              })
-            : singleArtist;
-        return (
-          <List.Item key={v4()} className="b-track-details-list-item">
-            {singleTrack.position}
-            . {artists} - {singleTrack.title}
-            <Button
-              animated="fade"
-              size="mini"
-              color="blue"
-              inverted
-              style={{ marginLeft: "0.5em" }}
-              className="b-track-details-list-item-button"
-              onClick={() =>
-                sendReleaseToTracklist(
-                  artists,
-                  singleTrack.title,
-                  label,
-                  track.id,
-                  singleTrack.position
-                )}
-            >
-              <Button.Content hidden>Add</Button.Content>
-              <Button.Content visible>
-                <Icon name="plus" />
-              </Button.Content>
-            </Button>
-          </List.Item>
-        );
-      })}
-    </div>
-  );
 }
 
 TrackDetailsDropdown.propTypes = {
   track: PropTypes.object.isRequired,
-  addReleaseToTracklist: PropTypes.func.isRequired,
   label: PropTypes.array.isRequired
 };
 
